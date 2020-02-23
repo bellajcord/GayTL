2991b1cf3938a68e75a554ce37f1d8f29d98f064
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-23 13:12:46 -0500

    new rest controller

--- ga-sei/project2/controllers/restController.js
+++ ga-sei/project2/controllers/restController.js
@@ -1,60 +1,31 @@
-/* Step 1 import express
- *
- */
+// Step 1 import express
+
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
+// Step 2 Import the api files from the models
+
 const restApi = require('../models/rest.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
+// Step 3  Create a new router.
+ 
 const restRouter = express.Router()
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
-
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
+// Step 4 TODO: Put all request handlers here
+ 
 //a GET index route that sends all restaurant to rest/index.hbs
 restRouter.get('/', (req, res) => {
   restApi.find().then(rest => {
     console.log(health);
-    res.render('rest/restHealth', {rest})
+    res.render('rest/rest', {rest})
   });
 });
 
-//index route that creates new restaurant
-restRouter.post('/index', (req, res) => { 
-  restApi.create(req.body).then(() => {
-    res.redirect('/');
-  });
+// a get route that shows newRest.hbs
+restRouter.get('/newRest' (req, res) => {
+  res.render('rest/newRest')
 });
 
-//a route /newRest that renders the newRest.hbs form
-restRouter.get('/newRest', (req, res) => {
-  res.render('rest/newRest');
-});
+
 /* Step 6
  *
  * Export the router from the file.
