708fca53310a7b108c3428b8c6c154a84c8f4c85
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-23 14:56:37 -0500

    created routes for health

--- ga-sei/project2/models/health.js
+++ ga-sei/project2/models/health.js
@@ -19,4 +19,41 @@ const healthSchema = new Schema({
 
 });
 
-module.exports = mongoose.model('health', healthSchema);
+//export the schema
+let healthCollection = mongoose.model('Health', healthSchema)
+
+//function to het all health
+
+function getAllHealth() {
+    return healthCollection.find()
+}
+
+//get a single health by id
+
+function getHealthById(healthID) {
+    return healthCollection.findById({_id: healthId})
+}
+
+//function to create new health
+function addNewHealth(health) {
+    return healthCollection.create(health)
+}
+
+//update health
+function updateHealth(healthId, health) {
+    return healthCollection.updateOne({_id: healthId}, health)
+}
+
+//delete health
+function deleteHealth(healthId) {
+    return healthCollection.deleteOne({_id: healthId})
+}
+
+//export created functions
+module.exports = {
+    getAllHealth,
+    getHealthById,
+    addNewHealth,
+    updateHealth,
+    deleteHealth
+}
