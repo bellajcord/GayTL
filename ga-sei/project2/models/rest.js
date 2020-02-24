d6b4a174310bc3c5e00dac70798e99d58f33bfb9
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-24 13:52:12 -0500

    fixed rest model

--- ga-sei/project2/models/rest.js
+++ ga-sei/project2/models/rest.js
@@ -1,8 +1,8 @@
-const mongoose = require('./connection.js');
+const mongoose = require('./connection');
 
 const  Schema  = mongoose.Schema;
 
-const restSchema = new Schema({
+const Rest = new Schema({
     name: {
         type: String,
         required: true,
@@ -19,41 +19,7 @@ const restSchema = new Schema({
 
 });
 
-//export the schema
-let restCollection = mongoose.model('Rest', restSchema)
 
-//function to het all restaurants
-
-function getAllRest() {
-    return restCollection.find()
-}
-
-//get a single rest by id
-
-function getRestById(restId) {
-    return restCollection.findById({_id: restId})
-}
-
-//function to create new rest
-function addNewRest(rest) {
-    return restCollection.create(rest)
-}
-
-//update rest
-function updateRest(restId, rest) {
-    return restCollection.updateOne({_id: restId}, rest)
-}
-
-//delete rest
-function deleteRest(restId) {
-    return restCollection.deleteOne({_id: restId})
-}
 
 //export created functions
-module.exports = {
-    getAllRest,
-    getRestById,
-    addNewRest,
-    updateRest,
-    deleteRest
-}
+module.exports = mongoose.model('Rest', Rest)
