f8a81be6656a558b5db37b98ae2b6d4632c12590
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-27 08:48:20 -0500

    sadfczv

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const { homeRouter } = require('./controllers/homeController.js')
 /* Step 3.a
  * ...to parse the body of the HTTP requests from a URL encoded string 
  */
-app.use(express.static(__dirname + '/public/'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: true}))
 
 /* Step 3.b 
